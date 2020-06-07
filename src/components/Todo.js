1bed884b8de96d1558857dd7155e1911eb61e9e8
keerthimac <keerthi.mac@gmail.com>
2020-06-07 10:07:47 +0530

    Todo & TodoItem convert complected

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import TodoItem from './TodoItem';
 import PropTypes from 'prop-types';
 
-class Todo extends Component {
-  render() {
-    // console.log(this.props.todos);
-    return this.props.todos.map((todo) => <TodoItem key={todo.id} todo={todo} markComplete={this.props.markComplete} delTodo={this.props.delTodo} />);
-  }
-}
+const Todo = ({ todos, markComplete, delTodo }) => {
+  // console.log(this.props.todos);
+  return todos.map((todo) => <TodoItem key={todo.id} todo={todo} markComplete={markComplete} delTodo={delTodo} />);
+};
 
 //Prop types
 Todo.propTypes = {
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,30 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export class TodoItem extends Component {
-  getStyle = () => {
+const TodoItem = ({ todo, markComplete, delTodo }) => {
+  const getStyle = () => {
     return {
       background: '#f4f4f4',
       padding: '10px',
       borderBottom: '1px #ccc dotted',
-      textDecoration: this.props.todo.complected ? 'line-through' : 'none',
+      textDecoration: todo.complected ? 'line-through' : 'none',
     };
   };
 
-  render() {
-    const { id, title } = this.props.todo;
-    return (
-      <div style={this.getStyle()}>
-        <p>
-          <input type="checkBox" onChange={this.props.markComplete.bind(this, id)} /> {title}
-          <button style={btnStyle} onClick={this.props.delTodo.bind(this, id)}>
-            x
-          </button>
-        </p>
-      </div>
-    );
-  }
-}
+  const { id, title } = todo;
+
+  const onCheck = () => {
+    markComplete(id);
+  };
+
+  const onClick = () => {
+    delTodo(id);
+  };
+
+  return (
+    <div style={getStyle()}>
+      <p>
+        <input type="checkBox" onChange={onCheck} /> {title}
+        <button style={btnStyle} onClick={onClick}>
+          x
+        </button>
+      </p>
+    </div>
+  );
+};
 
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired,
